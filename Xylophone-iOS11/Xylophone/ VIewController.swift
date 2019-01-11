d55ace304870c8703cbd39b1916e40eb1e5245d4
shailp250 <34988167+shailp250@users.noreply.github.com>
2019-01-11 18:02:25 -0500

    Add files via upload

---
+++ Xylophone-iOS11/Xylophone/ VIewController.swift
@@ -0,0 +1,40 @@
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer : AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    var selectedSoundFileName = ""
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        selectedSoundFileName = soundArray[sender.tag - 1]
+        
+        print(selectedSoundFileName)
+        
+        playSound()
+        
+    }
+    
+    func playSound() {
+        
+        let soundURL = Bundle.main.url(forResource: selectedSoundFileName, withExtension: "wav")
+        
+        audioPlayer = try! AVAudioPlayer(contentsOf: soundURL!)
+        
+        audioPlayer.play()
+    }
+    
+  
+
+}
+
